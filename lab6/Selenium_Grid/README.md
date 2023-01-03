93f0f4b7c93537d7ef9d704667f75c5bd5f9be51
Rastko Gajanin <e11930500@student.tuwien.ac.at>
2023-01-03 19:58:16 +0100

    Add reports and update README.md

--- lab6/Selenium_Grid/README.md
+++ lab6/Selenium_Grid/README.md
@@ -1,29 +1,56 @@
 # Selenium Grid
 
-## How to start
+## How to start the grid and run the tests
 
-### Running on host (one) machine in standalone mode
+### Starting the grid
 
+#### Running on host (one) machine in standalone mode
+
+Change directory to `lab6/Selenium_Grid`
+
+Execute:
 ```shell
 java -jar selenium-server-4.7.2.jar standalone --max-sessions 3
 ```
-We limit the maximal number of concurrent sessions to 4.
+We limit the maximal number of concurrent sessions to 3 for performance reasons.
 
-### Running multiple containers 
+#### Running multiple Docker containers 
 
+Change directory to `lab6/Selenium_Grid`
+
+Execute:
 ```shell
 docker compose up
 ```
+### Running the tests
 
-Run Cucumber tests as in the previous labs.
+Run Cucumber tests as in the previous labs. (either with Maven/Gradle or directly in IntelliJ)
 
 ## Adaptations Made
 
-We firstly added the `SeleniumRemoteWebDriver` class, which is almost identical to the already implemented `SeleniumWebDriver` class, except that it uses a remote host (the **Hub**) to execute the tests.
+1. We firstly added the `SeleniumRemoteWebDriver` class, which is almost identical to the already implemented `SeleniumWebDriver` class, except that it uses a remote host (the **Hub**) to execute the tests.
 
-`SeleniumWebDriver` class has then been exchanged with the new class in `BugstoreStepDefinitions` class.
+2. `SeleniumWebDriver` class has then been exchanged with the new class in `BugstoreStepDefinitions` class.
 
-Since the parallel execution of cucumber tests is by default disabled. It had to be enabled with `@ConfigurationParameter` annotation.
+3. Since the parallel execution of cucumber tests is by default disabled. It had to be enabled with `@ConfigurationParameter` annotation.
 
-UI Available on http://localhost:4444/ui.
+4. In the new class `SeleniumRemoteWebDriver`, the `RemoteWebDriver` object was wrapped with `ThreadLocal` so that each Scenario has its own WebDriver. (Due to extremely sparse documentation and forum support, this required the greatest effort to solve.)
 
+**Selenium Grid UI is available on http://localhost:4444/ui.**
+
+Note: When testing on different browser with docker, the chrome nodes have to be exchanged with the corresponding browser type.
+
+### Further useful commands
+
+Start a hub on this machine:
+```shell
+java -jar selenium-server-4.7.2.jar hub
+```
+
+Register this machine as a node (After starting the hub):
+```shell
+java -jar selenium-server-4.7.2.jar node --port 5556 --hub http://localhost:4444 --max-sessions 2
+```
+
+### Resources
+_Helpful resource for creating the docker containers_: https://github.com/SeleniumHQ/docker-selenium
--- src/test/java/at/ac/tuwien/inso/swtesten/util/SeleniumRemoteWebDriver.java
+++ src/test/java/at/ac/tuwien/inso/swtesten/util/SeleniumRemoteWebDriver.java
@@ -1,34 +1,18 @@
 package at.ac.tuwien.inso.swtesten.util;
 
-import com.google.common.collect.ImmutableMap;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.Browser;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.remote.UnreachableBrowserException;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 public class SeleniumRemoteWebDriver {
 	private static ThreadLocal<RemoteWebDriver> webdriver = new ThreadLocal<>();
-	private static String defaultLocale = "en"; //use english as default
-
 	private static ConcurrentHashMap< String,WebDriver> drivers = new ConcurrentHashMap<String,WebDriver>();
 
 	public static RemoteWebDriver getRemoteDriver() throws MalformedURLException {
@@ -47,28 +31,6 @@ public class SeleniumRemoteWebDriver {
 		return webdriver.get();
 	}
 
-	private static RemoteWebDriver createChromeWebDriver() {
-		WebDriverManager.chromedriver().setup();
-
-		ChromeOptions options = new ChromeOptions();
-		if (StringUtils.isNoneBlank(defaultLocale)) {
-			options.setExperimentalOption("prefs", ImmutableMap.builder().put("intl.accept_languages", defaultLocale).build());
-		}
-		return new ChromeDriver(options);
-	}
-
-	private static RemoteWebDriver createFireFoxWebDriver() {
-		WebDriverManager.firefoxdriver().setup();
-
-		FirefoxOptions options = new FirefoxOptions();
-		if (StringUtils.isNoneBlank(defaultLocale)) {
-			FirefoxProfile profile = new FirefoxProfile();
-			profile.setPreference("intl.accept_languages", defaultLocale);
-			options.setProfile(profile);
-		}
-
-		return new FirefoxDriver(options);
-	}
 
 	public static void closeDriver(WebDriver driver) {
 		if(webdriver.get() == null) {
@@ -80,10 +42,6 @@ public class SeleniumRemoteWebDriver {
 			webdriver.set(null);
 		} catch (UnreachableBrowserException ignored) {
 		}
-	}
-
-	public static void setDefaultLocale(String defaultLocale) {
-		SeleniumRemoteWebDriver.defaultLocale = defaultLocale;
 	}
 
 }
